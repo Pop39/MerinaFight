75407c739019069e4098a9a033a89a7b2ca3903a
Pop39 <panot.le@ku.th>
2017-12-01 09:48:06 +0700

    a

--- core/src/com/panot/mygdx/game/MerinaFight.java
+++ core/src/com/panot/mygdx/game/MerinaFight.java
@@ -1,5 +1,8 @@
 package com.panot.mygdx.game;
 
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
+
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
@@ -7,21 +10,24 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class MerinaFight extends ApplicationAdapter {
-	SpriteBatch batch;
+	public SpriteBatch batch;
 	Texture img;
+	ImageIcon icon;
+	JLabel button;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
 		img = new Texture("thunder.jpg");
+		icon = new ImageIcon("fire.jpg");
+		button = new JLabel(icon);
+
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
-		batch.draw(img, 0, 0);
+		batch.draw(img, 50, 50);
 		batch.end();
 	}
 	
