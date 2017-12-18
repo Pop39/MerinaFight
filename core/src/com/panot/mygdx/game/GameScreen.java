cbff52ad695c98af34df0b86d8048f1c4e1003f7
Pop39 <panot.le@ku.th>
2017-12-19 04:58:16 +0700

    a

--- core/src/com/panot/mygdx/game/GameScreen.java
+++ core/src/com/panot/mygdx/game/GameScreen.java
@@ -1,6 +1,11 @@
 package com.panot.mygdx.game;
 
+import java.util.Random;
+
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.ScreenAdapter;
+import com.badlogic.gdx.Input.Keys;
+import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
@@ -14,9 +19,19 @@ public class GameScreen extends ScreenAdapter {
 	private Texture leafImg;
 	private Texture merin1Img;
 	private Texture merin2Img;
+	private Texture winP;
+	private Texture winC;
+	private Texture loseP;
+	private Texture loseC;
+	private Texture drawP;
+	private Texture drawC;
 	private long time;
 	private String yourScoreName;
-	public int score = 0;
+	public int Pcard ;
+	public int Ccard;
+	public int scoreP = 0;
+	public int scoreC = 0;
+
 
     
 	public GameScreen(MerinaFight merinaFight) {
@@ -27,36 +42,164 @@ public class GameScreen extends ScreenAdapter {
         leafImg = new Texture("leaf.png");
         merin1Img = new Texture("merin1.png");
         merin2Img = new Texture("merin2.png");
+        winP = new Texture("win2.png");
+        winC = new Texture("win1.png");
+        loseP = new Texture("lose2.png");
+        loseC = new Texture("lose1.png");
+        drawP = new Texture("draw2.png");
+        drawC = new Texture("draw1.png");
     }
     
     @Override
 	public void render (float delta) {
         SpriteBatch batch = merinaFight.batch;
         BitmapFont yourBitmapFontName = merinaFight.yourBitmapFontName;
-        yourScoreName = "score: " + score;
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        yourScoreName = "score: " + scoreP + ":" + scoreC;
         update(delta);
         batch.begin();
-        batch.draw(thunderImg, 100, 50);
+        if(Pcard == 1) {
+        	batch.draw(leafImg, 300,300);
+        }
+        else if(Pcard == 2) {
+        	batch.draw(fireImg, 300,300);
+        }
+        else if(Pcard == 3) {
+        	batch.draw(waterImg, 300,300);
+        }
+        else if(Pcard == 4) {
+        	batch.draw(thunderImg, 300,300);
+        }
+        if(Ccard == 1) {
+        	batch.draw(leafImg, 500,300);
+        }
+        else if(Ccard == 2) {
+        	batch.draw(fireImg, 500,300);
+        }
+        else if(Ccard == 3) {
+        	batch.draw(waterImg, 500,300);
+        }
+        else if(Ccard == 4) {
+        	batch.draw(thunderImg, 500,300);
+        }
+        if(Pcard == Ccard && Pcard != 0 && Ccard!= 0) {//same element
+			batch.draw(drawP, 0,300);
+			batch.draw(drawC, 800,300);
+		}
+		else {
+			if(Pcard == Ccard+1) {//win
+				batch.draw(winP, 0,300);
+				batch.draw(loseC, 800,300);
+			}
+			else if(Pcard == 1 && Ccard == 4) {//win
+				batch.draw(winP, 0,300);
+				batch.draw(loseC, 800,300);
+			}
+			else if(Pcard == Ccard+2) {//opposite element will accident
+				batch.draw(loseP, 0,300);
+				batch.draw(loseC, 800,300);
+			}
+			else if(Pcard == Ccard-2 && Pcard>2) {//opposite element will accident
+				batch.draw(loseP, 0,300);
+				batch.draw(loseC, 800,300);
+			}
+			else if(Pcard == Ccard-1){//lose
+				batch.draw(loseP, 0,300);
+				batch.draw(winC, 800,300);
+			}
+			else if(Pcard == 4 && Ccard == 1){//lose
+				batch.draw(loseP, 0,300);
+				batch.draw(winC, 800,300);
+			}
+		}
+        
+        batch.draw(leafImg, 100, 50);
         batch.draw(fireImg, 300, 50);
         batch.draw(waterImg, 500, 50);
-        batch.draw(leafImg, 700, 50);
-        batch.draw(merin1Img, 200, 300);
-        batch.draw(merin2Img, 600, 300);
-        yourBitmapFontName.draw(batch, yourScoreName, 25, 100);
+        batch.draw(thunderImg, 700, 50);
+        batch.draw(merin1Img, 100, 300);
+        batch.draw(merin2Img, 700, 300);
+        yourBitmapFontName.draw(batch, yourScoreName, 450, 600);
         batch.end();
     }
     
     public void update(float delta) {
-    	score += merinaFight.fight();
         time = System.currentTimeMillis();
-
-    	if(merinaFight.isPress == true) {
-    		System.out.println("score" + score);//how to create score board ?
-    		merinaFight.isPress = false;
-    		while(System.currentTimeMillis() < time+500) {
+    	if(player() == true) {
+    		com();
+    		fight();
+    		System.out.println("score" + scoreP + ":" + scoreC);//how to create score board ?
+    		while(System.currentTimeMillis() < time+400) {
     			;
     		}
     	}
-
     }
+    
+	public void fight() {
+		if(Pcard == Ccard) {//same element
+			scoreP += 0;
+			scoreC += 0;
+		}
+		else {
+			if(Pcard == Ccard+1) {//win
+				scoreP += 1;
+				scoreC += -1;
+			}
+			else if(Pcard == 1 && Ccard == 4) {//win
+				scoreP += 1;
+				scoreC += -1;
+			}
+			else if(Pcard == Ccard+2) {//opposite element will accident
+				scoreP += -1;
+				scoreC += -1;
+			}
+			else if(Pcard == Ccard-2 && Pcard>2) {//opposite element will accident
+				scoreP += -1;
+				scoreC += -1;
+			}
+			else if(Pcard == Ccard-1 && Pcard != 1){//lose
+				scoreP += -1;
+				scoreC += 1;
+			}
+			else if(Pcard == 4 && Ccard == 1){//lose
+				scoreP += -1;
+				scoreC += 1;
+			}
+			else{//bug
+				scoreP += 0;
+				scoreC += 0;
+			}
+		}
+		
+	}
+	
+	public boolean player() {
+		if(Gdx.input.isKeyPressed(Keys.LEFT)) {
+			Pcard = 1;
+	        return true;//leaf
+	    }
+		else if(Gdx.input.isKeyPressed(Keys.RIGHT)) {
+			Pcard = 2;
+	        return true;//fire
+	    }
+		else if(Gdx.input.isKeyPressed(Keys.UP)) {
+			Pcard = 3;
+	        return true;//water
+	    }
+		else if(Gdx.input.isKeyPressed(Keys.DOWN)) {
+			Pcard = 4;
+	        return true;//thunder
+	    }
+		else{
+			return false;
+		}
+	}
+	
+	public void com() {
+		Random random = new Random();
+		int Comrandom = 1 + random.nextInt(3);
+		Ccard = Comrandom;
+	}
+
+    
 }
--- core/src/com/panot/mygdx/game/MerinaFight.java
+++ core/src/com/panot/mygdx/game/MerinaFight.java
@@ -13,7 +13,6 @@ public class MerinaFight extends Game {
 	public SpriteBatch batch;
 	public BitmapFont yourBitmapFontName;
 //	private String yourScoreName;
-	public boolean isPress = false;
 
 	@Override
 	public void create () {
@@ -33,61 +32,8 @@ public class MerinaFight extends Game {
 	@Override
 	public void dispose () {
         batch.dispose();
+        yourBitmapFontName.dispose();
         
 	}
 	
-	public int fight() {
-		if(player() == com()) {//same element
-			return 0;
-		}
-		else {
-			if(player() == com()+1) {//win
-				return 1;
-			}
-			else if(player() == com()+2) {//opposite element will accident
-				return -1;
-			}
-			else if(player() == com()-1 && player() != 0){//lose
-				return -1;
-			}
-			else{//bug
-				return 0;
-			}
-		}
-		
-	}
-	
-	public int player() {
-		if(Gdx.input.isKeyPressed(Keys.LEFT)) {
-			isPress = true;
-	        return 1;
-	    }
-		else if(Gdx.input.isKeyPressed(Keys.RIGHT)) {
-			isPress = true;
-	        return 2;
-	    }
-		else if(Gdx.input.isKeyPressed(Keys.UP)) {
-			isPress = true;
-	        return 3;
-	    }
-		else if(Gdx.input.isKeyPressed(Keys.DOWN)) {
-			isPress = true;
-	        return 4;
-	    }
-		else{
-			isPress = false;
-			return 0;
-		}
-	}
-	
-	public int com() {
-		Random random = new Random();
-		int Comrandom = 1 + random.nextInt(3);
-		return Comrandom;
-	}
-	
-	public void Tenthsubclass() {//this is 10th sub class
-		
-	}
-
 }
